b65327d4651bc83eb6e5aea21285a5d56c7e5237
justSid26 <lamasidharth25@gmail.com>
2025-09-29 23:57:32 +0530

    fixed some issues with quick sort

--- sorting/quickSort.java
+++ sorting/quickSort.java
@@ -1,39 +1,66 @@
-import java.util.*;
-class quickSort{
-	public static int partition(int nums[], int left, int right){
-		int pivot = nums[left - (right - left)/2];
-		int i = left -1;
-		int j = right +1;
-		while(true){
+package sorting;
 
-			do{
+public class quickSort {
+
+	public static int partition(int[] arr, int low, int high) {
+		
+		// Choose the middle element as pivot
+		int pivot = arr[(low + high) / 2];
+		int i = low - 1;
+		int j = high + 1;
+
+		while (true) {
+			// Increment i while arr[i] < pivot
+			do {
 				i++;
-			}while(nums[i] < pivot);
+			} while (arr[i] < pivot);
 
-			do{
+			// Decrement j while arr[j] > pivot
+			do {
 				j--;
-			}
-			while(nums[j] > pivot);
+			} while (arr[j] > pivot);
 
-			if(i >= j) return j;
+			// If pointers cross, return j
+			if (i >= j)
+				return j;
 
-			int temp = nums[i];
-			nums[i] = nums[j];
-			nums[j] = temp;
+			// Swap arr[i] and arr[j]
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
 		}
 	}
-	public static void sort(int nums[], int left, int right){
-		int pi = partition(nums, left, right);
-		quickSort(nums, low, pi);
-		quickSort(nums, pi+1, right);
+
+	// Main function to sort an array using QuickSort
+	public static void sort(int[] arr, int low, int high) {
+		if (low < high) {
+			
+			// pi is the partitioning index
+			int pi = partition(arr, low, high);
+			
+			// Recursively sort elements before and after partition
+			sort(arr, low, pi);
+			sort(arr, pi + 1, high);
+		}
 	}
-	public static void printElement(int nums[]){
-		for(int i = 0; i < nums.length; i++){
-			System.out.print(nums[i] + " ");
+
+	// To print sorted array
+	public static void printArr(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
 		}
 		System.out.println();
 	}
-	public static void main(String args[]){
-		int nums[] = {4,1,6,8,10};
+
+	public static void main(String[] args) {
+		int[] arr = {10,8,12,1,4,6};
+		int n = arr.length;
+
+		printArr(arr);
+		
+		sort(arr, 0, n - 1);
+		
+		System.out.println("Sorted array:");
+		printArr(arr);
 	}
 }
